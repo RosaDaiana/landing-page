aa5ba501899f1772ff91f880818216ae0780955f
Daiana Cabrel <daianacabrel376@gmail.com>
2025-01-22 16:28:46 -0500

    llamada a la api e integracion a la hoja html

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,4 +1,7 @@
 const url = 'https://youtube-v31.p.rapidapi.com/search?channelId=UCBJNpcJaUcVyw4LlqGRMpcQ&part=snippet%2Cid&order=date&maxResults=9';
+
+const content = null || document.getElementById('content');
+
 const options = {
 	method: 'GET',
 	headers: {
@@ -17,29 +20,23 @@ async function fetchData(urlApi) {
     try {
         const videos = await fetchData(url);
         let view = `
-        ${videos.items.map}
+        ${videos.items.map(video => `
             <div class="group relative">
                 <div
                     class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
-                    <img src="" alt="" class="w-full">
+                    <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
                 </div>
                 <div class="mt-4 flex justify-between">
                     <h3 class="text-sm text-gray-700">
                     <span aria-hidden="true" class="absolute inset-0"></span>
-                    title
+                    ${video.snippet.title}
                     </h3>
                 </div>
             </div>
-        `;
+            `).slice(0, 4).join('')}
+            `;
+            content.innerHTML = view;
     }catch(error){
-        console.error(error);
+        console.log(error);
     }
 })();
-
-// try {
-// 	const response = await fetch(url, options);
-// 	const result = await response.text();
-// 	console.log(result);
-// } catch (error) {
-// 	console.error(error);
-// }
